69ca7fb8c80032eb84f94c77187246628acff601
Fan5 <openconvenience@gmail.com>
2020-11-25 23:49:53 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,27 @@ const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController')
 module.exports = (app, passport) => {
+  //user authentication and admin authentiacation
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
   //show all restaurants
-  app.get('/', (req, res) => { res.redirect('/restaurants') })
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/', authenticated, (req, res) => { res.redirect('/restaurants') })
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   //admin
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   //user signup
   app.get('/signup', userController.signUpPage)
