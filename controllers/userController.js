2273b006aa0a77d83730254776979279936c8fca
Fan5 <openconvenience@gmail.com>
2020-12-02 17:09:16 +0800

    add edit profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -51,6 +51,31 @@ const userController = {
     let user = await User.findByPk(req.params.id)
     user = user.toJSON()
     res.render('editProfile', { user })
+  putUser: async (req, res) => {
+    const { name } = req.body
+    const image = req.file
+
+    //name is required
+    if (!name) {
+      req.flash('error_messages', '名稱不能為空白')
+      return res.redirect('back')
+    }
+
+    //if image file exists, upload to imgur; else, update name
+    if (image) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(image.path, async (err, img) => {
+        const user = await User.findByPk(req.params.id)
+        await user.update({ name: name, image: image ? img.data.link : null })
+        req.flash('success_messages', `成功修改${user.name}`)
+        return res.redirect(`/users/${req.params.id}`)
+      })
+    } else {
+      const user = await User.findByPk(req.params.id)
+      await user.update({ name })
+      req.flash('success_messages', `成功修改${user.name}`)
+      return res.redirect(`/users/${req.params.id}`)
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -64,4 +64,5 @@ module.exports = (app, passport) => {
   //users
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
+  app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 }
