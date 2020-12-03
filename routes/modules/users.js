b95c9808228754ff50e284e93083cbcd6a915b9e
Fan5 <openconvenience@gmail.com>
2020-12-03 20:46:36 +0800

    fix signin failure redirect route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,7 +10,7 @@ const { authenticated, isOwnProfile, editOwnProfile } = require('../../middlewar
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/users/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 router.get('/:id', authenticated, isOwnProfile, userController.getUser)
 router.get('/:id/edit', authenticated, editOwnProfile, userController.editUser)
